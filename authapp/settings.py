852d3d0db91bf36600693d5741768a0b8686c995
JoyMbugua <mbuguajoy52@gmail.com>
2021-09-28 14:59:59 +0300

    connect backend with react

--- authapp/settings.py
+++ authapp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3rd parties
+    'rest_framework',
+    #local
+    'authusers',
+    'frontend'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authusers.CustomUser'
---
+++ frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component} from "react";
+
+
+class App extends Component{
+  render(){
+    return(
+        <div className="site">
+            <h1>Hello From React Land!</h1>
+    </div>
+    );
+  }
+}
+
+export default App;
