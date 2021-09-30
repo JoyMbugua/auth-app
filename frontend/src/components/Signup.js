bb879eabeb0c592c4faa88171621a7395add1a4a
JoyMbugua <mbuguajoy52@gmail.com>
2021-09-30 14:15:29 +0300

    add email confirmation

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -10,8 +10,10 @@ class Signup extends Component {
     super(props);
 
     this.state = {
-      username: "",
-      email: "",
+        username: "",
+        email: "",
+        email2: "",
+        emailMismatch: ""
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -25,22 +27,26 @@ class Signup extends Component {
   async handleSubmit(event) {
     event.preventDefault();
     try {
-      const user = {
-        username: this.state.username,
-        email: this.state.email,
-      };
-      await axios
+      const emailBool = this.state.email === this.state.email2
+
+      if(emailBool){
+        await axios
         .post("http://127.0.0.1:8000/api/v1/users/register/", {
           username: this.state.username,
           email: this.state.email,
         })
         .then((res) => res)
         .then((result) => {
-            if (result.status === 201) {
-              console.log(result)
+          if (result.status === 201) {
+            console.log(result);
             window.location.replace("http://127.0.0.1:8000/verify/");
           }
         });
+      }else{
+          this.setState({
+              emailMismatch: "The two password fields didn’t match."
+          })
+      }
     } catch (error) {
       console.log(error.stack);
       this.setState({
@@ -51,29 +57,36 @@ class Signup extends Component {
   render() {
     return (
       <div>
-          <form onSubmit={this.handleSubmit}>
-            <CSRFToken />
-            <fieldset>
-              <legend>Sign Up</legend>
-              <input
-                name="username"
-                placeholder="Username"
-                type="text"
-                value={this.state.username}
-                onChange={this.handleChange}
-              />
+        <form onSubmit={this.handleSubmit}>
+          <CSRFToken />
+          <fieldset>
+            <legend>Sign Up</legend>
+            <p className="errormsg">{this.state.emailMismatch}</p>
+            <input
+              name="username"
+              placeholder="Username"
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
 
-              <input
-                placeholder="Email Address"
-                name="email"
-                type="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-              />
+            <input
+              placeholder="Email Address"
+              name="email"
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+            <input
+             placeholder="Confirm Email"
+            name="email2"
+            type="email"
+            value={ this.state.email2 }
+            onChange={this.handleChange}
+            />
             <input type="submit" value="Submit" onClick={this.shareData} />
-            </fieldset>
-          </form>
-      
+          </fieldset>
+        </form>
       </div>
     );
   }
